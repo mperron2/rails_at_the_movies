e9f9bb5c2370ac5e0669e4d4fde2fa33a0cbe5e0
maperron <ma.perron24@gmail.com>
2024-10-22 12:47:48 -0500

    Added associations to genres

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,6 @@
 class Genre < ApplicationRecord
+  has_many :movie_genre
+  has_many :movie, through: :movie_genre
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,9 @@
 class Movie < ApplicationRecord
   belongs_to :production_company
 
+  has_many :movie_genre
+  has_many :genres, through: :movie_genres
+
   validates :title, :year, :duration, :description, :average_vote, presence: true
   validates :year, :duration, numericality: { only_integer: true }
   validates :average_vote, numericality: true

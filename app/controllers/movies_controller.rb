9414b4c900c547b6f7b565290294042c64a25cec
maperron <ma.perron24@gmail.com>
2024-10-21 21:24:38 -0500

    Updated the Movie show page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.includes(:production_company).order("average_vote DESC")
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 end
--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,11 @@
 class ProductionCompaniesController < ApplicationController
   def index
+    # fetch production companies
+    @production_companies = ProductionCompany.all
   end
 
   def show
+    # fetch individual production company
+    @production_company = ProductionCompany.find(params[:id])
   end
 end
